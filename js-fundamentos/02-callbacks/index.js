e353af5f85de34fd2bcd47d38dd9d93c3b33ba0e
Alex Ballera <alexballera@gmail.com>
2020-11-29 13:25:25 -0300

    Manejo de errores

--- js-fundamentos/02-callbacks/index.js
+++ js-fundamentos/02-callbacks/index.js
@@ -4,26 +4,49 @@ const PEOPLE_URL = 'people/:id';
 const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`;
 const opts = {crossDomain: true};
 
-const onPeopleResponse = persona =>
-    console.log(`Hola, yo soy ${persona.name}`)
+// const onPeopleResponse = persona =>
+//     console.log(`Hola, yo soy ${persona.name}`)
 
 function obtenerPersonaje(id, callback) {
     const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+
+    // $
+    // .get(url, opts, callback)
+    // .fail(() => {
+    //     console.log(`Ocurrió un error! no se pudo obtener el personaje ${id}`);
+    // })
     fetch(url)
         .then(response => response.json())
-        .then(onPeopleResponse);
+        .then(callback)
+        .catch(error => {
+            console.log(`Hubo un problema con la petición Fetch, error: ${error.message}, no se pudo obtener el personaje ${id}`);
+          });
     
-    if (callback) {
-        callback()
-    }
+    // if (callback) {
+    //     callback()
+    // }
 }
-obtenerPersonaje(1, function() {
-    obtenerPersonaje(2, function() {
-        obtenerPersonaje(3, function() {
-            obtenerPersonaje(4, function() {
-                obtenerPersonaje(5, function() {
-                    obtenerPersonaje(6, function() {
-                        obtenerPersonaje(7)
+obtenerPersonaje(1, function(persona) {
+    console.log(`Hola, yo soy ${persona.name}`)
+
+    obtenerPersonaje(2, function(persona) {
+        console.log(`Hola, yo soy ${persona.name}`)
+
+        obtenerPersonaje(3, function(persona) {
+            console.log(`Hola, yo soy ${persona.name}`)
+
+            obtenerPersonaje(4, function(persona) {
+                console.log(`Hola, yo soy ${persona.name}`)
+
+                obtenerPersonaje(5, function(persona) {
+                    console.log(`Hola, yo soy ${persona.name}`)
+
+                    obtenerPersonaje(6, function(persona) {
+                        console.log(`Hola, yo soy ${persona.name}`)
+                        
+                        obtenerPersonaje(7, function(persona) {
+                            console.log(`Hola, yo soy ${persona.name}`)
+                        })    
                     })
                 })
             })
