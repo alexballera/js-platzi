c1ab7ac964932167c7c60024716054c266896090
Alex Ballera <alexballera@gmail.com>
2020-11-29 14:25:47 -0300

    Resolviendo callbacks anidados con Promesas

--- js-fundamentos/02-callbacks/index.js
+++ js-fundamentos/02-callbacks/index.js
@@ -4,60 +4,58 @@ const PEOPLE_URL = 'people/:id';
 const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`;
 const opts = {crossDomain: true};
 
-// const onPeopleResponse = persona =>
-//     console.log(`Hola, yo soy ${persona.name}`)
-
 function obtenerPersonaje(id, callback) {
-    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    return new Promise((resolve, reject) => {
+        const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+        // $
+        // .get(url, opts, function(data) {
+        //     resolve(data)
+        // })
+        // .fail(() => reject(id))
 
-    // $
-    // .get(url, opts, callback)
-    // .fail(() => {
-    //     console.log(`Ocurrió un error! no se pudo obtener el personaje ${id}`);
-    // })
-    fetch(url)
+        fetch(url)
         .then(response => response.json())
-        .then(callback)
-        .catch(error => {
-            console.log(`Hubo un problema con la petición Fetch, error: ${error.message}, no se pudo obtener el personaje ${id}`);
-          });
-    
-    // if (callback) {
-    //     callback()
-    // }
-}
-obtenerPersonaje(1, function(persona) {
-    console.log(`Hola, yo soy ${persona.name}`)
-
-    obtenerPersonaje(2, function(persona) {
-        console.log(`Hola, yo soy ${persona.name}`)
-
-        obtenerPersonaje(3, function(persona) {
-            console.log(`Hola, yo soy ${persona.name}`)
-
-            obtenerPersonaje(4, function(persona) {
-                console.log(`Hola, yo soy ${persona.name}`)
-
-                obtenerPersonaje(5, function(persona) {
-                    console.log(`Hola, yo soy ${persona.name}`)
-
-                    obtenerPersonaje(6, function(persona) {
-                        console.log(`Hola, yo soy ${persona.name}`)
-                        
-                        obtenerPersonaje(7, function(persona) {
-                            console.log(`Hola, yo soy ${persona.name}`)
-                        })    
-                    })
-                })
-            })
-        })
+        .then(data => resolve(data))
+        .catch(() => reject(id));
     })
-})
+}
 
-// Con JQuery
-// $.get(lukeUrl, opts, onPeopleResponse)
+function onError(id) {
+    console.log(`Sucedió un error al obtener al personaje ${id}`);
+}
 
-// Con Fetch
-// fetch(lukeUrl)
-// .then(response => response.json())
-// .then(onPeopleResponse);
+obtenerPersonaje(1)
+    .then(personaje =>{
+        console.log(`El personaje 1 es: ${personaje.name}`);
+
+        return obtenerPersonaje(2)
+    })
+    .then(personaje =>{
+        console.log(`El personaje 2 es: ${personaje.name}`);
+
+        return obtenerPersonaje(3)
+    })
+    .then(personaje =>{
+        console.log(`El personaje 3 es: ${personaje.name}`);
+
+        return obtenerPersonaje(4)
+    })
+    .then(personaje =>{
+        console.log(`El personaje 4 es: ${personaje.name}`);
+
+        return obtenerPersonaje(5)
+    })
+    .then(personaje =>{
+        console.log(`El personaje 5 es: ${personaje.name}`);
+
+        return obtenerPersonaje(6)
+    })
+    .then(personaje =>{
+        console.log(`El personaje 6 es: ${personaje.name}`);
+
+        return obtenerPersonaje(7)
+    })
+    .then(personaje =>{
+        console.log(`El personaje 7 es: ${personaje.name}`);
+    })
+    .catch(onError)
